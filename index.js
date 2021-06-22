056f4951056f7e2765498337ca27be87d7e9a184
kimdaehyun <zbeld123@gmail.com>
2021-06-22 19:17:53 +0900

    Added docker-compose.yml

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const redis = require("redis");
+const app = express();
+
+// redis client 설정
+const client = redis.createClient({
+  host: "redis-service",
+  ports: 6379,
+});
+
+const port = 8080;
+
+// 서버 구동시 증가될 숫자는 0으로 초기화 (redisDB에 저장)
+// redis는 key-value 쌍으로 데이터를 저장
+// key=num, value=0
+client.set("num", 0);
+
+app.get("/", (req, res) => {
+  client.get("num", (err, number) => {
+    // redis로부터 key값을 num으로 하는 값을 get
+    res.send("Incresed number from redis : " + number); // redis로 부터 get한 값을 그대로 브라우저에 응답
+    client.set("num", parseInt(number) + 1); // num을 key값으로 하는 value에 +1 만큼 증가시켜서 set(저장)
+  });
+});
+
+app.listen(port);
+console.log(`running on ${port}`);
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Docker 환경에서 multi container 구동
+
+- express, redis
+
+### 실행방법
+
+1. 컨테이너 구동: `docker-compose up` or `docker-compose up --build`
+2. 컨테이너 종료: `docker-compose down`
